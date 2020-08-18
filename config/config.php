fab2c005f89fb034fdb25a44a1eb22ff4e3654f5
TomasVotruba <tomas.vot@gmail.com>
2020-08-18 23:27:14 +0200

    colors

---
+++ config/config.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\PackageBuilder\Console\Style\SymfonyStyleFactory;
+use Symplify\SmartFileSystem\Finder\FinderSanitizer;
+use Symplify\SmartFileSystem\SmartFileSystem;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\ref;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+
+    $services->defaults()
+        ->public()
+        ->autowire()
+        ->autoconfigure();
+
+    $services->load('Migrify\EasyCI\\', __DIR__ . '/../src')
+        ->exclude([__DIR__ . '/../src/HttpKernel', __DIR__ . '/../src/ValueObject']);
+
+    $services->set(FinderSanitizer::class);
+    $services->set(SmartFileSystem::class);
+
+    $services->set(SymfonyStyleFactory::class);
+    $services->set(SymfonyStyle::class)
+        ->factory([ref(SymfonyStyleFactory::class), 'create']);
+};
