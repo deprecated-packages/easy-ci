eff69d54b20d3411e449211ae1c19ca4afebe809
Tomas Votruba <tomas.vot@gmail.com>
2023-07-18 10:25:35 +0200

    make use of misc

--- config/config.php
+++ config/config.php
@@ -10,11 +10,20 @@ use PhpParser\ParserFactory;
 use PhpParser\PrettyPrinter\Standard;
 use Symfony\Component\Console\Application;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCI\Config\Application\ClassAndConstantExistanceFileProcessor;
+use Symplify\EasyCI\Config\ConfigFileAnalyzer\NonExistingClassConfigFileAnalyzer;
+use Symplify\EasyCI\Config\ConfigFileAnalyzer\NonExistingClassConstantConfigFileAnalyzer;
+use Symplify\EasyCI\Config\Contract\ConfigFileAnalyzerInterface;
 use Symplify\EasyCI\Console\EasyCIApplication;
+use Symplify\EasyCI\Twig\Contract\TwigTemplateAnalyzerInterface;
+use Symplify\EasyCI\Twig\TwigTemplateAnalyzer\ConstantPathTwigTemplateAnalyzer;
+use Symplify\EasyCI\Twig\TwigTemplateAnalyzer\MissingClassConstantTwigAnalyzer;
+use Symplify\EasyCI\Twig\TwigTemplateProcessor;
 use Symplify\EasyCI\ValueObject\Option;
 use Symplify\PackageBuilder\Parameter\ParameterProvider;
 use Symplify\PackageBuilder\Reflection\ClassLikeExistenceChecker;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\tagged_iterator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
@@ -48,6 +57,21 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(Standard::class);
     $services->set(NodeFinder::class);
     $services->set(ClassLikeExistenceChecker::class);
+
+    // tagged services
+    $services->set(NonExistingClassConstantConfigFileAnalyzer::class)
+        ->tag(ConfigFileAnalyzerInterface::class);
+    $services->set(NonExistingClassConfigFileAnalyzer::class)
+        ->tag(ConfigFileAnalyzerInterface::class);
+    $services->set(ClassAndConstantExistanceFileProcessor::class)
+        ->args([tagged_iterator(ConfigFileAnalyzerInterface::class)]);
+
+    $services->set(ConstantPathTwigTemplateAnalyzer::class)
+        ->tag(TwigTemplateAnalyzerInterface::class);
+    $services->set(MissingClassConstantTwigAnalyzer::class)
+        ->tag(TwigTemplateAnalyzerInterface::class);
+    $services->set(TwigTemplateProcessor::class)
+        ->args([tagged_iterator(TwigTemplateAnalyzerInterface::class)]);
 
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::EXCLUDED_CHECK_PATHS, []);
