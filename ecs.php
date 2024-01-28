87011c9a81c757d1f59cee090dace122a206e4a9
Tomas Votruba <tomas.vot@gmail.com>
2024-01-29 00:23:30 +0100

    bump

--- ecs.php
+++ ecs.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
+return ECSConfig::configure();
+
 return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->paths([
         __DIR__ . '/config',
